5df7c64f949316557a4b667aa4a328ab31a29d7c
Caleb Melnychenko <calebinski98@gmail.com>
2025-12-12 19:44:20 -0500

    Caleb - Removed unnneeded files, updated App to be blank, and updated test file to reflect that.

--- app/src/tests/App.test.js
+++ app/src/tests/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Main testing', () => {
+
+  it('should have a main div in the application', () => {
+    render(<App />);
+
+    const mainDiv = screen.getByTestId('main');
+    
+    expect(mainDiv).toBeInTheDocument();
+  });
+
 });
